b123b8a701e231b79e46ae41119c54d487c60331
Filip Nedeljkovic <nedeljkovic.filip.dev@gmail.com>
2021-03-31 16:40:56 +0200

    Modified navbar and project structure

--- src/App.js
+++ src/App.js
@@ -2,22 +2,20 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import NavComponent from './components/nav/NavComponent';
 import React from 'react';
-import CarouselComponent from './components/carousel/CarouselComponent';
-import CardComponent from './components/cards/CardComponent';
-import css_cards from './CSS/css_cards.css'
-import aboutUs from './CSS/aboutUs.css'
-import {motion} from 'framer-motion'
-import AboutUsComponent from './components/about_us/AboutUsComponent';
+
+import {Route, Switch} from 'react-router-dom';
+
+import AboutUsComponent from './components/about_us/AboutUsComponent'
+import HomeComponent from './components/home/HomeComponent'
+
 function App() {
   return (
     <React.Fragment>
       <NavComponent/>
-      <CarouselComponent/>
-      <div className = "container-fluid">
-        <CardComponent/>
-        <br/>
-      </div>
-      <AboutUsComponent/>
+      <Switch>
+          <Route exact path="/" component={HomeComponent}/>
+          <Route exact path='/about' component={AboutUsComponent}/>
+      </Switch>
     </React.Fragment>
   );
 }
--- src/components/nav/NavComponent.js
+++ src/components/nav/NavComponent.js
@@ -1,19 +1,65 @@
-import React from 'react'
-import Nav from 'react-bootstrap/Nav'
-import Navbar from 'react-bootstrap/Navbar'
+import React, { useEffect, useState } from 'react'
+
+import './NavComponent.css'
+import { makeStyles } from '@material-ui/core/styles';
+
+import { AppBar, IconButton, Toolbar as MaterialToolbar, Typography, Box, Button, Link, List, ListItem, ListItemText, Fade  } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+        color: 'black'
+    },
+    title: {
+        flexGrow: 1,
+        color: 'black'
+    },
+    appBarSolid: {
+        transition: 'all 0.3s ease',
+        backgroundColor: 'rgba(255, 255, 255 ,0.5)',
+    },
+    appBarTransparent: {
+        transition: 'all 0.3s ease',
+        backgroundColor: 'rgba(255, 255, 255)'
+    }
+}));
+
+
 
 
 function NavComponent() {
+    
+    const classes = useStyles();
+
+    const [navBackground, setNavBackground] = useState('appBarTransparent')
+    const navRef = React.useRef()
+    navRef.current = navBackground
+    useEffect(() => {
+        const handleScroll = () => {
+            const show = window.scrollY > 50
+            if (show) {
+                setNavBackground('appBarSolid')
+            } else {
+                setNavBackground('appBarTransparent')
+            }
+        }
+        document.addEventListener('scroll', handleScroll)
+        return () => {
+            document.removeEventListener('scroll', handleScroll)
+        }
+    }, [])
+
     return (   
         <>
-            <Navbar bg = "dark" variant="dark" style = {{opacity: "87%"}} onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}>
-                <Navbar.Brand href="#home"></Navbar.Brand>
-                <Nav className="mr-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#features">About</Nav.Link>
-                <Nav.Link href="#pricing">Albums</Nav.Link>
-                </Nav>
-            </Navbar>
+            <AppBar className={classes[navRef.current]} position='fixed'>
+                <MaterialToolbar>
+                    <Link href='/' color='secondary'>Home</Link>
+                    <Link href='/about' color='secondary'>About</Link>
+                </MaterialToolbar>
+            </AppBar>
         </>
     )
 }
