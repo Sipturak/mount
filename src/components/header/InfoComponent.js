d459f19fd769cb99099860c5a6a17e3c2cf05a30
Sipturak <31665749+Sipturak@users.noreply.github.com>
2021-04-13 16:46:52 +0200

    fix

--- src/components/header/InfoComponent.js
+++ src/components/header/InfoComponent.js
@@ -3,25 +3,15 @@ import React, { Fragment, useEffect } from 'react'
 
 export const InfoComponent = () => {
     return (
-        <>
-        <div className = "row">
-        <div className="col-sm-6"></div>
-        <div className="col-sm-6">
-            <p className="main-title">Serbia is waiting for you to explore it.In your own way.Soon.</p>
-            </div>
-            </div>
-            <div className = "row">
-            <div className="col-sm-6"></div>
-            <div className = "col-sm-6">
-            <p className="main-text">Close your eyes and embark on an imaginative journey to a green country,
-               where the Alps meet the Mediterranean and the Pannonian Plain. Let your mind
-               wander through vast green forests or charming towns. Set your imagination
-               free and descend deep into the mysterious underground world of the Karst
-               region or climb high to the peaks of the Alps. 
-               Take a moment to think about and plan your future holidays in Serbia.</p>
-               </div>
-               </div>
-        </>
+        <div className="info">
+        <p className="main-title">Serbia is waiting for you to explore it.In your own way.Soon.</p>
+        <p className="main-text">Close your eyes and embark on an imaginative journey to a green country,
+           where the Alps meet the Mediterranean and the Pannonian Plain. Let your mind
+           wander through vast green forests or charming towns. Set your imagination
+           free and descend deep into the mysterious underground world of the Karst
+           region or climb high to the peaks of the Alps. 
+           Take a moment to think about and plan your future holidays in Serbia.</p>
+    </div>
     )
 }
 
