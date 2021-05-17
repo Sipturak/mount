007e812cd654f8e25f4093c6fce40a3c77c3f03f
nedeljkovicfilipdev <47796110+nedeljkovicfilipdev@users.noreply.github.com>
2021-05-17 17:24:00 +0200

    Populate donate input

--- src/components/donate/DonateComponent.js
+++ src/components/donate/DonateComponent.js
@@ -2,6 +2,16 @@ import React from 'react'
 import './DonateComponent.css'
 
 function DonateComponent() {
+
+    const populate5 = () => {
+        var text = document.getElementById("amountText");
+        text.value = '5'
+    }
+    const populate10 = () => {
+        var text = document.getElementById("amountText");
+        text.value = '10'
+    }
+
     return (
         <div className="donate-sections">
             <div className="donation-box">
@@ -15,14 +25,14 @@ function DonateComponent() {
                     <input type="text" id="cardNumber" placeholder="Card number"/>
                     <input type="text" id="expireDate" placeholder="MM/YY"/>
                     <input type="text" id="threedigits" placeholder="CVC"/>
-                    <input type="text" id="Amount" placeholder="Amount"/>
+                    <input type="text" id="amountText" placeholder="Amount" defaultValue="something"/>
                 </div>
                 <div className="donate-button">
                     <i className="fa fa-credit-card">Donate Now</i> 
                 </div>
                 <div className="amount">
-		            <div className="button">$5</div>
-		            <div className="button">$10</div>
+		            <div className="button" onClick={() => populate5()}>$5</div>
+		            <div className="button" onClick={() => populate10()}>$10</div>
 		            <div className="button">$25</div>
                     <div className="button">$35</div>
 		            <div className="button">$50</div>
